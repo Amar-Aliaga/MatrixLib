044d53cc72a60be9b20007a99484575894b87e39
rama <amaraliaga1@gmail.com>
2025-08-04 15:14:28 +0200

    added simple threading to matrix operations in main

--- app/main.cpp
+++ app/main.cpp
@@ -1,22 +1,42 @@
-#include <vector>
 #include "matrix.hpp"
+#include <vector>
 #include <chrono>
-using namespace mxlib;
+#include <thread>
+#include <mutex>
+
+
 
 int main() {
 
-    Matrix one (10,10,10000000.3);
-    Matrix two (10,10,48110000.4);
+    mxlib::Matrix one     (5000, 5000, 110000);
+    mxlib::Matrix two     (5000, 5000, 110000);
+    mxlib::Matrix three   (5000, 5000, 110000);
+    mxlib::Matrix four    (4000, 4000, 30000);
+    mxlib::Matrix result  (5000, 5000);
+    mxlib::Matrix result1 (5000, 5000);
+    mxlib::Matrix result2 (4000, 4000);
+
 
     auto start = std::chrono::high_resolution_clock::now();
-    one += two;
+
+    std::thread t1([&]() { result  = one + two;   });
+    std::thread t2([&]() { result1 = one + three; });
+    std::thread t3([&]() { result2 = four + four; });
+
+    t1.join();
+    t2.join();
+    t3.join();
+
     auto end = std::chrono::high_resolution_clock::now();
     
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "Execution time: " << duration.count() << " ms" << std::endl;
 
-    std::cout << one;
+
+    std::cout <<  result;
+    std::cout << result1;
+
 
     return 0;
 }
